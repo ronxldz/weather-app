fa46b455c5006fa7c272f30128f7577cb62c50f0
ronxldz <erik.p.1208@icloud.com>
2023-10-28 18:13:45 -0400

    first steps in creating the weather app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  IoMdSunny,
+  IoMdRainy,
+  IoMdCloudy,
+  IoMdSnow,
+  IoMdThunderstorm,
+  IoMdSearch,
+} from "react-icons/io";
+import {
+  BsCloudHaze2Fill,
+  BsCloudDrizzleFill,
+  BsEye,
+  BsWater,
+  BsThermometer,
+  BsWind,
+} from "react-icons/bs";
+import { TbTemperatureCelsius } from "react-icons/tb";
+import { ImSpinner8 } from "react-icons/im";
+
+// api key
+const APIkey = "cf50950b8b03edd46e6d606fc07a4ed5";
 
 function App() {
-  return <div>Weather App</div>;
+  const [data, setData] = useState(null);
+  const [location, setLocation] = useState("Atlanta");
+
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`;
+
+    axios.get(url).then((res) => {
+      setData(res.data);
+    });
+  }, [location]);
+
+  // console.log(data);
+
+  if (!data) {
+    return (
+      <div>
+        <div>
+          <ImSpinner8 className="text-7xl animate-spin" />
+        </div>
+      </div>
+    );
+  }
+
+  let icon;
+  console.log(data.weather[0].main);
+
+  switch (data.weather[0].main) {
+    case "Clouds":
+      icon = <IoMdCloudy />;
+      break;
+    case "Haze":
+      icon = <BsCloudHaze2Fill />;
+      break;
+    case "Rain":
+      icon = <IoMdRainy />;
+      break;
+    case "Clear":
+      icon = <IoMdSunny />;
+      break;
+    case "Drizzle":
+      icon = <BsCloudDrizzleFill />;
+      break;
+    case "Snow":
+      icon = <IoMdSnow />;
+      break;
+    case "Thunderstorm":
+      icon = <IoMdThunderstorm />;
+      break;
+    case "Snow":
+      icon = <IoMdSnow />;
+      break;
+  }
+
+  return <div className="text-6xl">{icon}</div>;
 }
 
 export default App;
